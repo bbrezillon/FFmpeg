2483a239ac360421aab3c5d405de66955e113012
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-28 10:51:15 +0200

    fixup! v4l2: Add VP8 support

--- libavcodec/vp8-ctrls.h
+++ libavcodec/vp8-ctrls.h
@@ -1,7 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * TODO: Make sure structs have no holes
- * and are 4-byte aligned.
+ * These are the VP8 state controls for use with stateless VP8
+ * codec drivers.
+ *
+ * It turns out that these structs are not stable yet and will undergo
+ * more changes. So keep them private until they are stable and ready to
+ * become part of the official public API.
  */
 
 #ifndef _VP8_CTRLS_H_
@@ -9,7 +13,9 @@
 
 #include <linux/v4l2-controls.h>
 
-#define V4L2_CID_MPEG_VIDEO_VP8_FRAME_HDR (V4L2_CID_MPEG_BASE + 590)
+#define V4L2_PIX_FMT_VP8_FRAME v4l2_fourcc('V', 'P', '8', 'F') /* VP8 parsed frames */
+
+#define V4L2_CID_MPEG_VIDEO_VP8_FRAME_HDR (V4L2_CID_MPEG_BASE + 2000)
 #define V4L2_CTRL_TYPE_VP8_FRAME_HDR 0x301
 
 #define V4L2_VP8_SEGMNT_HDR_FLAG_ENABLED              0x01
@@ -21,18 +27,18 @@ struct v4l2_vp8_segment_header {
 	__s8 quant_update[4];
 	__s8 lf_update[4];
 	__u8 segment_probs[3];
-	__u8 flags;
+	__u32 flags;
 };
 
 #define V4L2_VP8_LF_HDR_ADJ_ENABLE	0x01
 #define V4L2_VP8_LF_HDR_DELTA_UPDATE	0x02
 struct v4l2_vp8_loopfilter_header {
-	__u8 type;
+	__u16 type;
 	__u8 level;
 	__u8 sharpness_level;
 	__s8 ref_frm_delta_magnitude[4];
 	__s8 mb_mode_delta_magnitude[4];
-	__u8 flags;
+	__u16 flags;
 };
 
 struct v4l2_vp8_quantization_header {
@@ -52,18 +58,26 @@ struct v4l2_vp8_entropy_header {
 	__u8 mv_probs[2][19];
 };
 
-#define V4L2_VP8_FRAME_HDR_FLAG_EXPERIMENTAL		0x01
-#define V4L2_VP8_FRAME_HDR_FLAG_SHOW_FRAME		0x02
-#define V4L2_VP8_FRAME_HDR_FLAG_MB_NO_SKIP_COEFF	0x04
+struct v4l2_vp8_entropy_coder_state {
+	__u8 range;
+	__u8 value;
+	__u8 bit_count;
+};
+
+#define V4L2_VP8_FRAME_HDR_FLAG_KEY_FRAME		0x01
+#define V4L2_VP8_FRAME_HDR_FLAG_EXPERIMENTAL		0x02
+#define V4L2_VP8_FRAME_HDR_FLAG_SHOW_FRAME		0x04
+#define V4L2_VP8_FRAME_HDR_FLAG_MB_NO_SKIP_COEFF	0x08
+
+#define VP8_FRAME_IS_KEY_FRAME(hdr) (!!(hdr->flags & V4L2_VP8_FRAME_HDR_FLAG_KEY_FRAME))
+
 struct v4l2_ctrl_vp8_frame_header {
-	/* 0: keyframe, 1: not a keyframe */
-	__u8 key_frame;
 	__u8 version;
 
 	/* Populated also if not a key frame */
 	__u16 width;
-	__u8 horizontal_scale;
 	__u16 height;
+	__u8 horizontal_scale;
 	__u8 vertical_scale;
 
 	struct v4l2_vp8_segment_header segment_header;
@@ -71,8 +85,8 @@ struct v4l2_ctrl_vp8_frame_header {
 	struct v4l2_vp8_quantization_header quant_header;
 	struct v4l2_vp8_entropy_header entropy_header;
 
-	__u8 sign_bias_golden;
-	__u8 sign_bias_alternate;
+	__u16 sign_bias_golden;
+	__u16 sign_bias_alternate;
 
 	__u8 prob_skip_false;
 	__u8 prob_intra;
@@ -80,26 +94,17 @@ struct v4l2_ctrl_vp8_frame_header {
 	__u8 prob_gf;
 
 	__u32 first_part_size;
-	__u32 first_part_offset;
-	/*
-	 * Offset in bits of MB data in first partition,
-	 * i.e. bit offset starting from first_part_offset.
-	 */
 	__u32 macroblock_bit_offset;
-
 	__u32 dct_part_sizes[8];
 	__u8 num_dct_parts;
 
-	__u8 bool_dec_range;
-	__u8 bool_dec_value;
-	__u8 bool_dec_count;
+	struct v4l2_vp8_entropy_coder_state coder_state;
 
-	/* v4l2_buffer timestamps of reference frames */
 	__u64 last_frame_ts;
 	__u64 golden_frame_ts;
 	__u64 alt_frame_ts;
 
-	__u8 flags;
+	__u64 flags;
 };
 
 #endif
